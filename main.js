1de530bb34f42bf2281d499b58a9cd5814c2acf5
penayoalbi <albi.penayo@gmail.com>
2021-05-06 13:50:04 -0300

    validar

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 document.getElementById("btn-registro").addEventListener("click",registrar);
 document.getElementById("btn-iniciarSesion").addEventListener("click",iniciarSesion);
+//document.getElementById("ingresar").addEventListener("click",validar);
 
 var form_registro=document.querySelector(".form-registro");
 var form_login_registro=document.querySelector(".form-login-registro");
@@ -21,5 +22,30 @@ function iniciarSesion(){
     form_login.style.display = "block";
     caja_registro.style.opacity = "1" ;//que no se muestre
     caja_login.style.opacity = "0";// que se muestre
+}
+//validar login
+function validar() {
+    var exp = /(?=.*\d)(?=.*[A-Z])/g;
+    var usuario = 0, clave = 0;
+    usuario = document.getElementById("log_usuario").value;
+    clave = document.getElementById("log_clave").value;
+    if( usuario===""){
+        alert("El campo usuario esta vacio.");
+        return false;
+    }else if(clave ===""){
+        alert("Clave esta vacio.");
+    }
+    else  if(!clave.match(exp)){
+         alert("El password Debe tener al menos un numero y una letra mayuscula.");
+    }
+    else{
+       return true;
+    }  
+    window.onload = function(){
+        var contenedor = document.getElementById("contenedor");
+        contenedor.style.visibility ='hidden';
+        contenedor.style.opacity = '0';
+    }
+      
 }
 
