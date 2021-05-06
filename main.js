bfa6a4c90906d4ad61d66d3f8c8c3362a80814df
penayoalbi <albi.penayo@gmail.com>
2021-05-06 02:33:25 -0300

    animación de login-registro n

--- main.js
+++ main.js
@@ -1,26 +1,27 @@
-document.getElementsById("btn-iniciar-sesion").addEventListener("click",iniciarSesion);
-document.getElementsById("btn-registro").addEventListener("click",registrar);
 
-var from_login_registro=document.querySelector(".form-login-registro");
-var from_login=document.querySelector(".form-login");
-var from_registro=document.querySelector(".form-registro");
+document.getElementById("btn-registro").addEventListener("click",registrar);
+document.getElementById("btn-iniciarSesion").addEventListener("click",iniciarSesion);
 
+var form_registro=document.querySelector(".form-registro");
+var form_login_registro=document.querySelector(".form-login-registro");
+var form_login=document.querySelector(".form-login");
 var caja_login=document.querySelector(".caja-login");
 var caja_registro=document.querySelector(".caja-registro");
 
 function registrar(){ 
-      from_registro.style.display = "block";
-      from_login_registro.style.style.left = "410px"; 
-      from_login.style.display = "none";
-      caja_registro.style.opacity = "0" ;//que no se muestre
-      caja_login.style.opacity = "1";// que se muestre
+      form_registro.style.display ="block";
+      form_login_registro.style.left ="410px"; 
+      form_login.style.display ="none";
+      caja_registro.style.opacity ="0" ;//que no se muestre
+      caja_login.style.opacity ="1";// que se muestre
+    }
 
-}
 function iniciarSesion(){ 
-    from_registro.style.display = "none";
-    from_login_registro.style.style.left = "10px"; 
-    from_login.style.display = "block";
+    form_registro.style.display = "none";
+    form_login_registro.style.left = "10px"; 
+    form_login.style.display = "block";
     caja_registro.style.opacity = "1" ;//que no se muestre
     caja_login.style.opacity = "0";// que se muestre
 
 }
+
