31cf9c48565b8749a6b708862fd46feee7493225
penayoalbi <albi.penayo@gmail.com>
2021-05-27 23:05:34 -0300

    config

---
+++ index.js
@@ -0,0 +1,42 @@
+addEventListener("load",load);
+var miServidor = "/proyetoEdi/back/";
+
+function load(){
+    mensajeAlServido(miServidor,cargarRegistro);
+}
+
+function cargarRegistro(){
+
+}
+
+function mensajeAlServido(servidor, funcionArealizar)
+{
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.open("GET", servidor, true);
+     //seteo el evento
+     xmlhttp.onreadystatechange = function () {
+        //Veo si llego la respuesta del servidor
+        if (xmlhttp.readyState == XMLHttpRequest.DONE) {
+            //Reviso si la respuesta es correcta
+            if (xmlhttp.status == 200) {
+                funcionArealizar(xmlhttp.responseText);
+            }
+            else {
+                alert("ocurrio un error");
+            }
+        }
+    }
+
+    //envio el mensaje    
+    xmlhttp.send();
+}
+
+/*
+function load(){
+    $("ingresar").click(function(){
+        $.post("index.php",function(datos){
+            $("resultado").html(datos);
+        })
+    })
+    alert("esto es login");
+}*/
