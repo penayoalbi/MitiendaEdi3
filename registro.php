e257a18446f7932ac64a686e8ab4529422b1a580
penayoalbi <albi.penayo@gmail.com>
2021-06-03 18:10:39 -0300

    registro

--- registro.php
+++ registro.php
@@ -1,11 +1,5 @@
 <?php
- /*
- $nombre = $_GET['txtnombre'];
- $apellido = $_GET['apellido'];
- $correo = $_GET['txtcorreo'];
- $pass = $_GET['txtpass'];
- $repass = $_GET['txtrepass'];
-*/
+ 
 echo "Hola mundo";
  function crearRegistro(){
     $nombre = $_POST['txtnombre'];
@@ -16,25 +10,10 @@ echo "Hola mundo";
 
     $arch = fopen('registro.txt','w');
 
-    fwrite($arch,$nombre);
-    fwrite($arch,$apellido);
+    fwrite($arch,$nombre.",");
+    fwrite($arch,$apellido );
     fclose($arch);
-
 }
- 
- crearRegistro();
-
- /*<?php //Ejemplo aprenderaprogramar.com, archivo escribir.php
-
-$file = fopen("archivo.txt", "w");
-
-fwrite($file, "Esto es una nueva linea de texto" . PHP_EOL);
-
-fwrite($file, "Otra más" . PHP_EOL);
-
-fclose($file);
+crearRegistro();
 
 ?>
- */
-
-?> 
