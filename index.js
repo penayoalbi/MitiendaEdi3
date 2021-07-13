188946e98b90fe7b08e941d765f2849c9fc18fb3
penayoalbi <albi.penayo@gmail.com>
2021-07-12 22:42:25 -0300

    serv en heroku

--- index.js
+++ index.js
@@ -1,30 +1,25 @@
-addEventListener("load",load)
+addEventListener("load",load);
 var miServidor = "https://apipenayo.herokuapp.com"
-//var miServidor ="http://localhost:666";
+//var miServidor ="http://localhost:666/usuario";
 
 function load(){
-   //mensajeAlServido(miServidor,funcionArealizar);
- document.getElementById("btnIngresar").addEventListener("click",login);
- //document.getElementById("btnRegistrar").addEventListener("click",EnviarMensajeServidor);
- 
+    //mensajeAlServido(miServidor,funcionArealizar);
+    document.getElementById('btnIngresar').addEventListener("click",login);
+    //document.getElementById("registro").addEventListener("click",EnviarMensajeServidor);
 }
+
 function funcionArealizar(){
    console.log(xmlhttp);
 }
-/*
-function login(miServidor, funcionArealizar)
-{
-   EnviarMensajeServidor(miServidor,funcionArealizar);
-}
-*/
+
 function login (miServidor, funcionArealizar)
 {
     var xmlhttp = new XMLHttpRequest();
     var datos = new FormData();
     datos.append('nombre',$('nombre').value);
     datos.append('clave',$('clave').value);
-
-    xmlhttp.open('get',miServidor+'/usuario/login', true);
+//'http://localhost:666/usuario/login'
+    xmlhttp.open('get',miServidor, true);
      //seteo el evento
      xmlhttp.onreadystatechange = function () {
         //Veo si llego la respuesta del servidor
@@ -32,40 +27,23 @@ function login (miServidor, funcionArealizar)
             //Reviso si la respuesta es correcta
             if (xmlhttp.status == 200) {
                 //funcionArealizar(xmlhttp.responseText);
-                 console.log( xmlhttp.responseText);   
-            }
-            else {
+                 console.log( xmlhttp.responseText); 
+                 $('#form-login').trigger('reset');
+                 document.getElementById('respuesta').innerHTML=xmlhttp.responseText; 
+                }else {
                 alert("ocurrio un error");
+                document.getElementById('respuesta').innerHTML=xmlhttp.responseText;
             }
         }
     }
-   xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-   xmlhttp.setRequestHeader("enctype", "multipart/form-data");
- 
+   //xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xmlhttp.setRequestHeader("enctype", "multipart/form-data");
     //envio el mensaje  
     xmlhttp.send(datos);
 }
-/*
-function BuscarProducto(){
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.open('post','http://localhost:666/Producto',true);
 
-    xmlhttp.onreadystatechange = function () {
-    if (xmlhttp.readyState == XMLHttpRequest.DONE) {
-         //Reviso si la respuesta es correcta
-         if (xmlhttp.status == 200) {
-            funcionArealizar(xmlhttp.responseText);
-        }
-        else {
-            alert("ocurrio un error en el peticion");
-        }
-    }
 
-    xmlhttp.send();
-}
-}
-*/
-
+//crear usuario
 function EnviarMensajeServidor (miServidor, funcionArealizar)
 {
     var xmlhttp = new XMLHttpRequest();
@@ -73,8 +51,8 @@ function EnviarMensajeServidor (miServidor, funcionArealizar)
     datos.append('nombre',$('nombre').value);
     datos.append('nombre',$('correo').value);
     datos.append('clave',$('clave').value);
-
-    xmlhttp.open('post',miServidor+'/usuario/new', true);
+//'http://localhost:666/usuario/new'
+    xmlhttp.open('post',miServidor, true);
      //seteo el evento
      xmlhttp.onreadystatechange = function () {
         //Veo si llego la respuesta del servidor
@@ -89,37 +67,42 @@ function EnviarMensajeServidor (miServidor, funcionArealizar)
             }
         }
     }
-   xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  // xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
    xmlhttp.setRequestHeader("enctype", "multipart/form-data");
  
     //envio el mensaje  
     xmlhttp.send(datos);
 }
 
-/*
-function enviarLogin(){
-    $("#form-login").on("submit", function(e){
+$('#registro').click(function(e){
+    console.log('click en registro');
+    var xmlhttp = new XMLHttpRequest();
+    var registro = new FormData();
+    registro.append('nombre',$('nombre').value);
+    registro.append('nombre',$('correo').value);
+    registro.append('clave',$('clave').value);
+    //'http://localhost:666/usuario/new'
+    xmlhttp.open('post',miServidor, true);
+    //seteo el evento
+    xmlhttp.onreadystatechange = function () {
+       //Veo si llego la respuesta del servidor
+       if (xmlhttp.readyState == XMLHttpRequest.DONE) {
+           //Reviso si la respuesta es correcta
+           if (xmlhttp.status == 200) {
+               //funcionArealizar(xmlhttp.responseText);
+                console.log( xmlhttp.responseText);   
+           }
+           else {
+               alert("ocurrio un error");
+           }
+       }
+   }
+ // xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  xmlhttp.setRequestHeader("enctype", "multipart/form-data");
 
-        //cancelar comportamiento 
-        e.preventDefaul();
-        //tomar datos del form
-        var form= $(this);
-        var formData = new FormData(document.getElementById("form-login"));
-        formData.append("datos","valor");
-        xmlhttp.onreadystatechange = function () {
-            if (xmlhttp.readyState == XMLHttpRequest.DONE) {
-                 //Reviso si la respuesta es correcta
-                 if (xmlhttp.status == 200) {
-                    funcionArealizar(xmlhttp.responseText);
-                }
-                else {
-                    alert("ocurrio un error");
-                }
-            }
-    });
-
-}
+   //envio el mensaje  
+   xmlhttp.send(registro);
 
 });
-*/
+
 
---
+++ producto.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+    console.log('producto');
+
+
+});
+
+/*
+function BuscarProducto(){
+    $('#btnBuscar').preventDafaul();
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.open('post','http://localhost:666/Producto',true);
+
+    xmlhttp.onreadystatechange = function () {
+    if (xmlhttp.readyState == XMLHttpRequest.DONE) {
+         //Reviso si la respuesta es correcta
+         if (xmlhttp.status == 200) {
+            funcionArealizar(xmlhttp.responseText);
+        }
+        else {
+            alert("ocurrio un error en el peticion");
+        }
+    }
+    xmlhttp.send();
+    }
+}
+*/
